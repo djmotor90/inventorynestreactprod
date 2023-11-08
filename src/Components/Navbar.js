0723dd0c8df245b34653d2313c5345fb8008ee6f
Gricelg517 <135929737+Gricelg517@users.noreply.github.com>
2023-11-07 20:34:42 -0600

    add components

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import{ Link } from "react-router-dom";
+
+
+export default function Navbar() {
+    return (
+        <>
+            <Link to='/'>Home</Link>
+            <Link to='/new'>New</Link>
+            
+        </>
+    )
+}
