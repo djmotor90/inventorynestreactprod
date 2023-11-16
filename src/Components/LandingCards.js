7e95a8c22a12853a64c27624cd812b8413aa5d26
akullyot <akullyot@gmail.com>
2023-11-15 21:01:18 -0600

    I was having merging problems so I took Rhionnas work that she did and committed it here

--- src/Components/LandingCards.js
+++ src/Components/LandingCards.js
@@ -11,62 +11,62 @@ import Button    from 'react-bootstrap/Button';
 
 function LandingCard({ data }) {    
     const navigate = useNavigate();
-    const handleViewAllCustomersClick = () =>{navigate(`/customers`);};
-    const handleViewAllWarehousesClick = () =>{navigate(`/warehouses`);};
-    const handleViewAllSalesClick = () =>{navigate(`/sales`);};
-    const handleViewPerformanceAnalyticsClick = () =>{navigate(`/reporting`);};
+    const handleViewAllCustomers = () =>{navigate(`/customers`);};
+    const handleViewAllWarehouses = () =>{navigate(`/warehouses`);};
+    const handleViewAllSales = () =>{navigate(`/sales`);};
+    const handleViewPerformanceAnalytics = () =>{navigate(`/reporting`);};
    return(
-        <ListGroup horizontal data-bs-theme="dark">
-            <ListGroup.Item>
-                <Card>
-                    <Card.Img variant="top" src="https://placehold.co/600x400" />
-                    <Card.Body>
-                        <Card.Title> Customers </Card.Title>
-                        <Card.Text>
-                                Currently you are serving {data.customerCount} customers across the United States.
-                        </Card.Text>
-                        <Button variant="btn btn-primary" onClick={() => handleViewAllCustomersClick()}> View All Customers </Button>
-                    </Card.Body>
-                </Card>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <Card>
-                    <Card.Img variant="top" src="https://placehold.co/600x400" />
-                    <Card.Body>
-                        <Card.Title> Warehouses </Card.Title>
-                        <Card.Text>
-                                Currently you are running {data.warehouseCount} warehouses across the United States.
-                        </Card.Text>
-                        <Button variant="btn btn-primary" onClick={() => handleViewAllWarehousesClick()}> View All Warehouses </Button>
-                    </Card.Body>
-                </Card>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <Card>
-                    <Card.Img variant="top" src="https://placehold.co/600x400" />
-                    <Card.Body>
-                        <Card.Title> Sales </Card.Title>
-                        <Card.Text>
-                                In total, you have made {data.salesCount} individual product sales.
-                        </Card.Text>
-                        <Button variant="btn btn-primary" onClick={() => handleViewAllSalesClick()}> View All Sales </Button>
-                    </Card.Body>
-                </Card>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <Card>
-                    <Card.Img variant="top" src="https://placehold.co/600x400" />
-                    <Card.Body>
-                        <Card.Title> Revenue </Card.Title>
-                        <Card.Text>
-                                Grossly, you have made {data.revenueCount} dollars
-                        </Card.Text>
-                        <Button variant="btn btn-primary" onClick={() => handleViewPerformanceAnalyticsClick()}> View Performance Analytics </Button>
-                    </Card.Body>
-                </Card>
-            </ListGroup.Item>
-        </ListGroup>
-    );
-    }
+    <ListGroup horizontal data-bs-theme="dark">
+    <ListGroup.Item>
+        <Card className="h-100 d-flex flex-column">
+            <Card.Img variant="top" src="https://placehold.co/600x400" />
+            <Card.Body className="flex-grow-1">
+                <Card.Title> Customers </Card.Title>
+                <Card.Text>
+                    Currently you are serving {data.customerCount} customers across the United States.
+                </Card.Text>
+            </Card.Body>
+            <Button variant="btn btn-primary" onClick={() => handleViewAllCustomers()}> View All Customers </Button>
+        </Card>
+    </ListGroup.Item>
+    <ListGroup.Item>
+        <Card className="h-100 d-flex flex-column">
+            <Card.Img variant="top" src="https://placehold.co/600x400" />
+            <Card.Body className="flex-grow-1">
+                <Card.Title> Warehouses </Card.Title>
+                <Card.Text>
+                    Currently you are running {data.warehouseCount} warehouses across the United States.
+                </Card.Text>
+            </Card.Body>
+            <Button variant="btn btn-primary" onClick={() => handleViewAllWarehouses()}> View All Warehouses </Button>
+        </Card>
+    </ListGroup.Item>
+    <ListGroup.Item>
+        <Card className="h-100 d-flex flex-column">
+            <Card.Img variant="top" src="https://placehold.co/600x400" />
+            <Card.Body className="flex-grow-1">
+                <Card.Title> Sales </Card.Title>
+                <Card.Text>
+                    In total, you have made {data.salesCount} individual product sales.
+                </Card.Text>
+            </Card.Body>
+            <Button variant="btn btn-primary" onClick={() => handleViewAllSales()}> View All Sales </Button>
+        </Card>
+    </ListGroup.Item>
+    <ListGroup.Item>
+        <Card className="h-100 d-flex flex-column">
+            <Card.Img variant="top" src="https://placehold.co/600x400" />
+            <Card.Body className="flex-grow-1">
+                <Card.Title> Revenue </Card.Title>
+                <Card.Text>
+                    Grossly, you have made {data.revenueCount} dollars.
+                </Card.Text>
+            </Card.Body>
+            <Button variant="btn btn-primary" onClick={() => handleViewPerformanceAnalytics()}> View Performance </Button>
+        </Card>
+    </ListGroup.Item>
+</ListGroup>
+);
+}
 
 export default LandingCard;
